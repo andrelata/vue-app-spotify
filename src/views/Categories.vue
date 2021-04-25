657cb20a385937e0b3314832699c18b927e816f8
Andrea Lata <andrelata12@gmail.com>
2021-04-25 19:22:40 -0300

    add image

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="categories">
     <h1>Categories</h1>
-    <div v-for="item in items" v-bind:key="item" >
-      {{ item.name }}
+    <div v-for="item in categories" :key="item.id">
+      <img  :src="item.icons.url" 
+            :style="{ 'height' : item.icons.height , 
+                      'width' : item.icons.width}">
+      <h2>{{ item.name }}</h2>
     </div>
   </div>
 </template>
@@ -17,28 +20,26 @@ export default {
   name: 'categories',
   data () {
     return {
-        items: [
+        categories: [
                 {
                     "href":"https://api.spotify.com/v1/browse/categories/toplists",
-                    "icons":[
+                    "icons":
                     {
                         "height":275,
                         "url":"https://datsnxq1rwndn.cloudfront.net/media/derived/toplists_11160599e6a04ac5d6f2757f5511778f_0_0_275_275.jpg",
                         "width":275
-                    }
-                    ],
+                    },
                     "id":"toplists",
                     "name":"Top Lists"
                 },
                 {
                     "href":"https://api.spotify.com/v1/browse/categories/mood",
-                    "icons":[
+                    "icons":
                     {
                         "height":274,
                         "url":"https://datsnxq1rwndn.cloudfront.net/media/original/mood-274x274_976986a31ac8c49794cbdc7246fd5ad7_274x274.jpg",
                         "width":274
-                    }
-                    ],
+                    },
                     "id":"mood",
                     "name":"Mood"
                 }
